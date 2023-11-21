c924cb5945d1ea2436667327ef55da4c66b8981f
krish <sharmageeta847@gmail.com>
2023-11-22 01:10:21 +0530

    deployment

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ const connect = async () => {
       console.log(error);
     }
   };
-  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+  app.use(cors({ origin: "https://api-h2x3.onrender.com", credentials: true }));
   app.use(express.json());
   app.use(cookieParser());
 
