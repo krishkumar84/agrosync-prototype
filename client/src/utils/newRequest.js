a056db0baad73dc62fafbf9fab5d9bae663dfdbe
krish <sharmageeta847@gmail.com>
2024-06-20 21:46:12 +0530

    rate limit

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const newRequest = axios.create({
   //baseURL: "http://localhost:3000/api/",
   // baseURL: "https://api-h2x3.onrender.com/api/",
-  baseURL: "http://ec2-3-109-48-113.ap-south-1.compute.amazonaws.com:3000/api/",
+   baseURL: "https://krrish.me/api/",
   withCredentials: true,
 });
 
