32b110b629e92f18dbbb956e40bfdfae3edd844f
krish <sharmageeta847@gmail.com>
2024-06-30 14:16:46 +0530

    solving vercel err

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const newRequest = axios.create({
   //baseURL: "http://localhost:3000/api/",
   // baseURL: "https://api-h2x3.onrender.com/api/",
-   baseURL: "https://krrish.me/api/",
+  //  baseURL: "https://krrish.me/api/",
+   baseURL: "https://agrosync-backend.vercel.app/api/",
   withCredentials: true,
 });
 
